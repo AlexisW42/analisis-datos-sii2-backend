80b5c3833946cfd19d781410de8c4f4f43eebcdf
freddyyohanvillamizar-pixel <freddyyohan.villamizar@unet.edu.ve>
2026-06-21 16:44:44 -0400

    feat: estructura inicial de rutas y validadores CU01

--- app/modules/carga/router.py
+++ app/modules/carga/router.py
@@ -1,7 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile, HTTPException
+from .service import ValidadorArchivo
 
 router = APIRouter(prefix="/carga", tags=["carga"])
+validador = ValidadorArchivo()
 
 @router.get("/")
 def get_carga():
     return {"message": "Módulo de Carga (CU01)"}
+
+# ---------------------------------------------------------
+# CU01: Cargar DataSet (Endpoint Principal)
+# ---------------------------------------------------------
+@router.post("/cargar")
+def cargar_dataset(file: UploadFile = File(...)):
+    """
+    Recibe un archivo, valida su formato, tamaño y estructura.
+    """
+    try:
+        validador.get_resultado(file)
+        
+        return {
+            "estadoCarga": "Exitoso",
+            "message": "Archivo validado con éxito.",
+            "archivoCargado": file.filename,
+            "tamañoBytes": file.size
+        }
+        
+    except ValueError as e:
+        # FLUJOS ALTERNOS / EXCEPCIONES: 
+        # Si el validador lanza un ValueError (formato inválido, muy pesado, dañado),
+        # lo atrapamos aquí y devolvemos un Error 400 (Bad Request) al FrontEnd.
+        raise HTTPException(status_code=400, detail=str(e))
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}")
